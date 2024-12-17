43b431c680db04aa4d80320c06a810b2dba75aa7
aebel-shajan <aebelshajan@gmail.com>
2024-12-17 21:07:18 +0000

    Add workout data calendar heatmap.

--- yd_website/src/pages/Home/Home.tsx
+++ yd_website/src/pages/Home/Home.tsx
@@ -1,6 +1,9 @@
 import { useEffect, useState } from "react";
 import styles from "./Home.module.css"
 import { fetchData } from "../../api/axiosClient";
+import CalHeatmap from 'cal-heatmap';
+import 'cal-heatmap/cal-heatmap.css';
+
 
 interface Workout {
   "index": number,
@@ -26,7 +29,24 @@ const Home = () => {
       try {
         const result = await fetchData<Workout[]>("/workout-data");
         setWorkoutData(result);
-        console.log(result)
+        const cal = new CalHeatmap();
+        cal.paint({
+          domain: {
+            type: "month"
+          },  
+          subDomain: {
+            type: "day"
+          },
+          date: {
+            start: new Date("2024-01-01")
+          },
+          data: {
+            source: result,
+            x: "Date",
+            y: "Volume",
+            groupY: "min"
+          }
+        });
       } catch (error) {
         console.error("Error fetching data:", error);
       }
@@ -44,6 +64,18 @@ const Home = () => {
         2024 is over, it was meh. Here is what I was doing this year represented through
         a yearly heatmap.
       </p>
+
+      <div
+      className={styles.workoutContainer}
+      >
+        <h2>Workout Activity</h2>
+        <div id="cal-heatmap"></div>
+        <p>
+          Damn I fell off. Although I feel like I've still been gaining muscle more now.
+          I started my first job near the end of April. You can see how the frequency
+          decreases after this point.
+        </p>
+      </div>
     </div>
    );
 }
