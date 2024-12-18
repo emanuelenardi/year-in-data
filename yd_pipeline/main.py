c86154b7cb1cc6f451842693101fdfbc063f6696
aebel-shajan <aebelshajan@gmail.com>
2024-12-18 13:16:09 +0000

    Add process_kindle_data to pipeline.

--- yd_pipeline/main.py
+++ yd_pipeline/main.py
@@ -1,9 +1,26 @@
-from yd_pipeline.process_strong_data import process_strong_data
+from yd_pipeline.process_data import (
+    process_strong_data,
+    process_kindle_data
+)
 from pathlib import Path
 
 root_dir = Path(__file__).resolve().parent
 
 data_file_path = root_dir / "data" / "input"
-strong_data_filepath = data_file_path /  "strong266140424475682467.csv"
+
+# ETL strong data
+strong_data_filepath = data_file_path / "strong266140424475682467.csv"
 with open(strong_data_filepath) as strong_csv:
     process_strong_data(strong_csv)
+
+# ETL kindle data
+kindle_data_filepath = (
+    data_file_path
+    / "Kindle"
+    / "Kindle.ReadingInsights"
+    / "datasets"
+    / "Kindle.reading-insights-sessions_with_adjustments"
+    / "Kindle.reading-insights-sessions_with_adjustments.csv"
+)
+with open(kindle_data_filepath) as kindle_csv:
+    process_kindle_data(kindle_csv)
--- yd_pipeline/yd_pipeline/process_data.py
+++ yd_pipeline/yd_pipeline/process_data.py
@@ -49,3 +49,52 @@ def process_strong_data(csv_file: BinaryIO):
     # Load pandas dataframe into sqlite table
     connection = sqlite3.connect('data/output/year_in_data.db')
     strong_df.to_sql('workout_data', connection, if_exists='replace')
+    
+
+def process_kindle_data(csv_file: BinaryIO):
+    """
+    Read in kindle data from csv file. 
+    Store fine grain data in year_in_data.kindle_data_fine_grain.
+    Store daily data in year_in_data.kindle_data_daily.
+
+    Parameters
+    ----------
+    csv_file : BinaryIO
+        Csv file containing kindle activity data. Has the following columns:
+        [ASIN,end_time,reading_marketplace,start_time,total_reading_milliseconds]
+    """
+    
+    # Read in csv from config into a pandas dataframe
+    kindle_df= pd.read_csv(
+        csv_file, 
+        delimiter=detect_delimiter(csv_file), 
+        parse_dates=["start_time", "end_time"]
+    )
+    
+    columns_to_keep = [
+        "ASIN",
+        "end_time",
+        "start_time",
+        "total_reading_milliseconds"
+    ]
+    if not check_columns_exist(kindle_df, columns_to_keep):
+        raise ValueError(
+            "CSV provided does not contain required columns!\n"
+            f"\t* CSV columns: {list(kindle_df.columns)}\n"
+            f"\t* Required columns: {columns_to_keep}"
+        )
+    
+    # Store fine grain data in seperate table for future use
+    connection = sqlite3.connect('data/output/year_in_data.db')
+    kindle_df.to_sql('kindle_data_fine_grain', connection, if_exists='replace')
+    
+    # For daily info
+    daily_kindle_df = kindle_df[["ASIN", "start_time", "total_reading_milliseconds"]]
+    daily_kindle_df["date"] = pd.to_datetime(
+        daily_kindle_df["start_time"],
+        format="ISO8601"
+    ).dt.date
+    daily_kindle_df = daily_kindle_df[["ASIN", "date", "total_reading_milliseconds"]]
+    daily_kindle_df = daily_kindle_df.groupby(["ASIN", "date"]).sum()
+    daily_kindle_df.to_sql('kindle_data_daily', connection, if_exists='replace')
+    
