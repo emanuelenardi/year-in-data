c3ef8c152378b597aa909c930b817d4e808db2b7
aebel-shajan <aebelshajan@gmail.com>
2024-12-24 17:22:59 +0000

    Move sleep heatmap into own component.

--- yd_website/src/pages/Home/Home.tsx
+++ yd_website/src/pages/Home/Home.tsx
@@ -1,28 +1,10 @@
-import { useEffect, useState } from "react";
 import styles from "./Home.module.css"
-import { fetchData } from "../../api/axiosClient";
-// @ts-expect-error cal-heatmap library don't have declration files :(
-import CalHeatmap from 'cal-heatmap';
-import { DataResponseType, SleepData } from "../../types/dataTypes";
-import {  drawSleepHeatmap } from "./heatmapUtils";
 import ReadingHeatmap from "./ReadingHeatmap/ReadingHeatmap";
 import GithubHeatmap from "./GithubHeatmap/GithubHeatmap";
 import WorkoutHeatmap from "./WorkoutHeatmap/WorkoutHeatmap";
+import SleepHeatmap from "./SleepHeatmap/SleepHeatmap";
 
 const Home = () => {
-  const [sleepCal,] = useState(new CalHeatmap())
-  useEffect(() => {
-    const getData = async () => {
-      try {
-        const sleepData = await fetchData<DataResponseType<SleepData[]>>("/sleep-data");
-        drawSleepHeatmap(sleepCal, sleepData)
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      }
-    };
-
-    getData();
-  }, [])
 
   return (
     <div
@@ -44,18 +26,7 @@ const Home = () => {
 
         <GithubHeatmap />
 
-        <div
-          className={styles.dataSection}
-        >
-          <h2>Sleep Activity (From Fitbit)</h2>
-          <div id="sleep-heatmap"></div>
-          <div id="sleep-legend"></div>
-          <p>
-            Sleep was much better than expected. Especially recently in December, I am
-            getting days when I sleep for 10 hours :0. Lol you can see when I lost my Fitbit
-            in February. I am also surprised at how often I have my fitbit on.
-          </p>
-        </div>
+        <SleepHeatmap />
       </div>
     </div>
   );
