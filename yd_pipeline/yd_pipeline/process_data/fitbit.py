86ac2faa91f9a090b896601537d04ff146d82201
aebel-shajan <aebelshajan@gmail.com>
2024-12-31 15:03:05 +0000

    Add abstract functions for processing fitbit data.

--- yd_pipeline/yd_pipeline/process_data/fitbit.py
+++ yd_pipeline/yd_pipeline/process_data/fitbit.py
@@ -5,6 +5,74 @@ import sqlite3
 from yd_pipeline.utils import validate_columns
 
 
+def extract_json_file_data(folder_path: str, file_name_prefix: str, keys_to_keep: str) -> pd.DataFrame:
+    """Extract fitbit data from the folder path containing jsons. The files in the folder
+    have the format like : "{file_name_prefix}-YYYY-MM-DD.json".
+
+    Parameters
+    ----------
+    folder_path : str
+        Path to folder containing jsons of fitbit data.
+    file_name_prefix : str
+        Defines what specific data to filter.
+    keys_to_keep : str
+        From the jsons, keys_to_keep determines which key value pairs should be kept.
+
+    Returns
+    -------
+    pd.DataFrame
+        Pandas dataframe with keys_to_keep as the columns with rows being objects/dicts 
+        extracted from the jsons.
+    """
+    file_names = [f for f in os.listdir(folder_path) if f.startswith(file_name_prefix)]
+    full_data = []
+    for file_name in file_names:
+        file_path = folder_path / file_name
+        with open(file_path) as file:
+            data_list = json.load(file)
+            for data in data_list:
+                filtered_data = {key: data[key] for key in keys_to_keep}
+                full_data.append(filtered_data)
+
+    return pd.DataFrame(full_data)
+
+
+def transform_time_series_data(df: pd.DataFrame) -> pd.DataFrame:
+    """Apply transformations to dataframe containing timeseries data.
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        dataframe containing columns:
+        * dateTime with type string.
+        * value with type integer.
+        
+    Returns
+    -------
+    pd.DataFrame
+        Dataframe containing columns:
+        * date with type datetime.
+        * value with type integer.
+    """
+    columns_to_keep = ["dateTime", "value"]
+    validate_columns(df, columns_to_keep)
+    df = df[columns_to_keep]
+    df = df.rename(
+        columns={
+            "dateTime": "date",
+        }
+    )
+    df.loc[:, "value"] = pd.to_numeric(df["value"])
+    df.loc[:, "date"] = pd.to_datetime(df["date"], format="%m/%d/%y %H:%M:%S").dt.date
+    df = df.groupby(["date"]).aggregate(
+        {
+            "value": "sum",
+        }
+    )
+    df["value"] = df["value"].astype(int)
+    return df
+
+
 def extract_sleep_data(folder_path: str):
     """Extract sleep data from files from the folder path. The files have the name format
     "sleep-YYYY-MM-DD.json".
