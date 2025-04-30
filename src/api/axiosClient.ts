36aa392c2bf4297b0a4da8aff4414ceb48989b71
aebel-shajan <aebelshajan@gmail.com>
2025-04-30 07:57:10 +0100

    Refactor axiosClient baseURL to use production server

--- src/api/axiosClient.ts
+++ src/api/axiosClient.ts
@@ -2,7 +2,8 @@ import axios from "axios";
 
 export const axiosInstance = axios.create({
   // baseURL: "https://aebelshajan.pythonanywhere.com/",
-  baseURL: "http://localhost:8000/",
+  // baseURL: "http://localhost:8000/",
+  baseURL: "http://67.205.140.3:8000",
   // timeout: 5000, // Set a timeout of 5 seconds
   headers: {
     "Content-Type": "application/json",
